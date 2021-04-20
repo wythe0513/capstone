d4e2f4dc119c484db0602819529b4df5662c6b1a
wythe0513 <wythenshawe0513@gmail.com>
2021-04-20 23:22:03 +0900

    revise

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
+curl --request POST \
+  --url https://tomascap.jp.auth0.com/oauth/token \
+  --header 'content-type: application/json' \
+  --data '{"client_id":"OYMVmHrMZa0vD9eNinhYvmRMVQ0iU9Oz","client_secret":"OYMVmHrMZa0vD9eNinhYvmRMVQ0iU9Oz","audience":"agency","grant_type":"client_credentials"}'
 
+export DOMAIN="tomascap.jp.auth0.com"
+export CLIENT_ID="OYMVmHrMZa0vD9eNinhYvmRMVQ0iU9Oz"
+export CLIENT_SECRET="OYMVmHrMZa0vD9eNinhYvmRMVQ0iU9Oz"
