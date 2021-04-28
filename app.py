b7162ac241c2807dae8143b0dfd8e4cd2d7d680c
wythe0513 <wythenshawe0513@gmail.com>
2021-04-29 00:48:11 +0900

    welcome endpoint add

--- app.py
+++ app.py
@@ -42,6 +42,12 @@ def create_app(test_config=None):
 # Routes
 # ---------------------------------------------------------
 
+    @app.route('/')
+    def get_greeting():
+        greeting = "Welcome to the Agency !! You can get JWT here." 
+        
+        return greeting
+
     # GET endpoint for list of actors in database.
     @app.route('/actors', methods=['GET'])
     def get_actors():
